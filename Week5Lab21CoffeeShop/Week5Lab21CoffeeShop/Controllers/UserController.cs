bd7e342a4493579451551edd631aa24936255f69
staffordc <stafford.carh@gmail.com>
2018-08-14 14:56:03 -0400

    added working html validation

---
+++ Week5Lab21CoffeeShop/Week5Lab21CoffeeShop/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Week5Lab21CoffeeShop.Models;
+
+namespace Week5Lab21CoffeeShop.Controllers
+{
+    public class UserController : Controller
+    {
+        
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        public ActionResult Create(User collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Week5Lab21CoffeeShop/Week5Lab21CoffeeShop/Models/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Week5Lab21CoffeeShop.Validators;
+
+namespace Week5Lab21CoffeeShop.Models
+{
+    public class User
+    {
+
+        [MinLength(2, ErrorMessage ="Do you have a longer version of that name?")]
+        [Required]
+        [MaxLength(50, ErrorMessage ="time to lawyer up against parents")]
+        [Display(Name = ("FirstName"))]
+        [RegularExpression("\\D+", ErrorMessage = "No numblos plz")]
+        public string FirstName { get; set; }
+
+        [MinLength(2, ErrorMessage = "No initals plz?")]
+        [Required]
+        [MaxLength(50, ErrorMessage ="time to lawyer up against parents")]
+        [Display(Name = ("LastName"))]
+        [RegularExpression("\\D+",ErrorMessage = "No numblos plz")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "please give me a different one")]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "more please")]
+        public string Password { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [Compare("Password", ErrorMessage ="lol, try again.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string FavoriteCoffee { get; set; }
+    }
+
+}
