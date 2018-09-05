17baf2cd7a83ed24d975d054c55b89d197e41028
staffordc <stafford.carh@gmail.com>
2018-09-04 21:11:04 -0400

    Working on Cart and ViewBags

--- Week5Lab21CoffeeShop/Week5Lab21CoffeeShop/Controllers/CartController.cs
+++ Week5Lab21CoffeeShop/Week5Lab21CoffeeShop/Controllers/CartController.cs
@@ -18,8 +18,38 @@ namespace Week5Lab21CoffeeShop.Controllers
         // GET: Cart
         public ActionResult Index()
         {
+            var UserLogin = Request.Cookies["UserLogin"];
+            var LoginValue = int.Parse(UserLogin.Value);
+            var user = db.Users.FirstOrDefault(u => u.UserId == LoginValue);
+            ViewBag.User = user;
+            //V Upset at last run because the set is Null?
+            ViewBag.Items = user.Items.ToList();
+            //Set up Dictionary for Count of Items
+            var ItemsCounts = user.Items.GroupBy(i => i.ItemId).ToDictionary(k => k.Key, v => v.Count());
             return View(db.Items);
         }
-        
+        public ActionResult Add(int? id)
+        {
+            if (id == null || Request.Cookies["UserLogin"] == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var UserLogin = Request.Cookies["UserLogin"];
+            var user = db.Users.FirstOrDefault(u => u.UserId == int.Parse(UserLogin.Value));
+
+            Item item = db.Items.Find(id);
+            if (user == null || item == null)
+            {
+                return HttpNotFound();
+            }
+
+            user.Items.Add(item);
+            db.SaveChanges();
+
+            return View(db.Items);
+            
+        }
+
     }
 }
